a1839839acfae4a5f270f15f3d068b7ab35400cb
Undead Dev <110062066+MRE08@users.noreply.github.com>
2022-10-06 10:48:02 +0100

    Forked.app.py

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ word = input('Type any word : ')
 for letter in word :
   if letter.lower() not in VOWEL :
     print(letter, end ="")
+    print('This letter is a consonant')
+
+  else:
+    print(letter, end="")
+    print('This letter is a vowel')
